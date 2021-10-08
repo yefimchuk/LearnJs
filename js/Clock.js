9617753f447309d69d46e821a4a356a99c2107dc
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-10-08 23:30:57 +0300

    learning Class checking: "instanceof"

--- js/Clock.js
+++ js/Clock.js
@@ -33,5 +33,5 @@ class Clock {
     }
 }
 let clock = new Clock({ template: "h:m:s"})
-clock.start()
 
+    clock.stop()
--- js/script.js
+++ js/script.js
@@ -1,1 +1,78 @@
-console.log("loc")
+/*
+
+class PowerArray extends Array {
+    isEmpty() {
+        return this.length ;
+    }
+}
+
+let arr = new PowerArray(1, 2, 5, 10, 50);
+console.log(arr.isEmpty()); // false
+
+let filteredArr = arr.filter(item => item >= 10);
+console.log(Symbol.species); // 10, 50
+console.log(filteredArr.isEmpty()); // fal*/
+
+/*
+class Mammal {
+    constructor(name) {
+        this.name = name;
+        _
+    }
+
+// Non-static method
+    eat() {
+        console.log(this.name + " eats");
+    }
+
+// static method
+    static sleep() {
+        console.log(this.name + " sleeps");
+    }
+}
+
+class Cat extends Mammal {
+// not writing any constructor relying on the default one.
+    walk() {
+        console.log(this.name + " walks");
+    }
+}
+
+let cat = new Cat("kitty");
+
+// Cat class own method - Works
+cat.walk(); // kitty walks
+// Non static method of parent class - Works
+cat.eat(); // kitty eats
+// Static method of parent class - Error!!
+cat.sleep(); // Error : not a function
+*/
+/*
+let obj = {
+
+}
+console.log(obj)*/
+
+/*
+class Rabbit {}
+class Rabbit2 {}
+let rabbit = new Rabbit();
+let rabbit2 = new Rabbit2();
+
+// is it an object of Rabbit class?
+console.log( rabbit instanceof Rabbit2 ); // true
+*/
+
+class Rabbit{}
+
+//a
+let die = ({}).toString.call(new Rabbit()) // Uncaught SyntaxError: Unexpected token '.'
+console.log(die)
+//but below works fine (Note : I haven't set Symbol.toStringTag - but I guess that shouldn't give the error)
+//b
+Object.prototype.toString.call(new Rabbit())// "[object Object]"
+//c
+var s = {}.toString;
+s.call(new Rabbit())//"[object Object]"
+//d
+console.log(Object.prototype.toString.call(new Rabbit()) === {}.toString.call(new Rabbit())) // returns true
