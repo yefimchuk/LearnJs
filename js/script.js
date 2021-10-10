2c3537a082a4ed069e8d9390b004970253ec21c4
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-10-11 00:47:56 +0300

    Custom errors, extending Error

--- js/script.js
+++ js/script.js
@@ -97,3 +97,18 @@ function readData() {
 
 readData();
 */
+
+class FormatError extends SyntaxError {
+    constructor(message) {
+        super(message);
+        this.name = "FormatError";
+    }
+}
+
+let err = new FormatError("ошибка форматирования");
+
+console.log( err.message ); // ошибка форматирования
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
+
+console.log( err instanceof SyntaxError ); // true
