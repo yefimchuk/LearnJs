91e9c95c959e0d194eb6d3ba1f90c69bfaa5078b
yefimchuk <dmitriy.yefimchuk@gmail.com>
2021-10-21 22:07:38 +0300

    learning Promise

--- js/script.js
+++ js/script.js
@@ -418,10 +418,43 @@ brick.onclick = function() {
 
 function delay(ms) {
     return new Promise(function(resolve, reject) {
-        resolve(1);
 
         setTimeout((ms) => resolve(2),ms);
     });
 }
 
 delay(3000).then(() => alert('runs after 3 seconds'));
+
+//===============================================
+
+
+function go() {
+    showCircle(150, 150, 100).then(div => {
+        div.classList.add('message-ball');
+        div.append("Hello, world!");
+    });
+}
+
+
+    function showCircle(cx, cy, radius, callback) {
+
+            let div = document.createElement('div');
+            div.style.width = 0;
+            div.style.height = 0;
+            div.style.left = cx + 'px';
+            div.style.top = cy + 'px';
+            div.className = 'circle';
+            document.body.append(div);
+            return new Promise(function(resolve, reject) {
+            setTimeout(() => {
+                div.style.width = radius * 2 + 'px';
+                div.style.height = radius * 2 + 'px';
+
+                div.addEventListener('transitionend', function handler() {
+                    div.removeEventListener('transitionend', handler);
+                    resolve(div)
+                });
+            }, 0);
+
+        })
+}
